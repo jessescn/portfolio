68ae19d733b066f6f7c4e2074c4b232aaa700f80
Jessé Souza <jesse.neto@ccc.ufcg.edu.br>
2021-04-04 11:17:23 -0300

    change folder build to .next.

--- next.config.js
+++ next.config.js
@@ -3,5 +3,4 @@ const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   // Use the CDN in production and localhost for development.
   assetPrefix: isProd ? 'https://cdn.statically.io/gh/jessescn/jessescn.github.io/gh-pages/' : '',
-  distDir: 'build'
 }
